b44407c760e22e495a48da22699a97c3cc7e82a7
rootimes <rootimes8596@gmail.com>
2026-01-15 16:55:08 +0800

    refactor: routes to simplify dashboard access and remove unused settings routes

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -7,7 +7,9 @@ use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
 {
-    protected static ?string $title = '資訊總覽';
+    protected static ?string $title = '總覽';
+
+    protected static ?string $navigationLabel = 'Home';
 
     public function getWidgets(): array
     {
